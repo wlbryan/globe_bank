d932676120ee5b213d45f7d9a2b13e126f785d35
Bill Bryan <wlbryan@wlbryan.net>
2018-09-04 18:38:37 -0400

    End of O8-03, display errors working properly

--- public/staff/subjects/new.php
+++ public/staff/subjects/new.php
@@ -2,6 +2,25 @@
 
 require_once('../../../private/initialize.php');
 
+if(is_post_request()) {
+
+  $subject = [];
+  $subject['menu_name'] = $_POST['menu_name'] ?? '';
+  $subject['position'] = $_POST['position'] ?? '';
+  $subject['visible'] = $_POST['visible'] ?? '';
+
+  $result = insert_subject($subject);
+  if($result === true) {
+  $new_id = mysqli_insert_id($db);
+  redirect_to(url_for('/staff/subjects/show.php?id=' . $new_id));    
+  } else {
+    $errors = $result;
+  }
+
+} else {
+  // display the blank form
+}
+  
 $subject_set = find_all_subjects();
 $subject_count = mysqli_num_rows($subject_set) + 1;
 mysqli_free_result($subject_set);
@@ -20,8 +39,10 @@ $subject["position"] = $subject_count;
 
   <div class="subject new">
     <h1>Create Subject</h1>
+    
+    <?php echo display_errors($errors); ?>
 
-    <form action="<?php echo url_for('/staff/subjects/create.php'); ?>" method="post">
+    <form action="<?php echo url_for('/staff/subjects/new.php'); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
         <dd><input type="text" name="menu_name" value="" /></dd>
