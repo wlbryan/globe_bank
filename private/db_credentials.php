6f6af79b55025f44b9d86412ed0ec43a4cb608f8
Bill Bryan <wlbryan@wlbryan.net>
2018-09-04 15:06:20 -0400

    db credentials updated

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,8 @@
 <?php
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "secretpassword");
+define("DB_USER", "homestead");
+define("DB_PASS", "secret");
 define("DB_NAME", "globe_bank");
 
 ?>
