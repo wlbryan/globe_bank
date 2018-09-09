755defe716a1f7a49250a1a704a47b4d8637da46
Bill Bryan <wlbryan@wlbryan.net>
2018-09-09 17:15:34 -0400

    Work on navigation from database

--- public/index.php
+++ public/index.php
@@ -1,5 +1,19 @@
 <?php require_once('../private/initialize.php'); ?>
 
+<?php 
+
+  if(isset($_GET['id'])) {
+    $page_id = $_GET['id'];
+    $page = find_page_by_id($page_id);
+    if(!$page) {
+      redirect_to(url_for('/index.php'));
+    }
+  } else {
+    // nothing selected, show the home page
+  }
+
+?>
+
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 
@@ -10,7 +24,19 @@
 
   <div id="page">
   
+    <?php 
     
+      if(isset($page)) {
+        // show the page from the database
+        // TODO - add HTML escaping back in
+        echo $page['content'];
+        
+      } else {
+        include(SHARED_PATH . '/static_homepage.php');    
+      }
+    
+    
+    ?>
   
   </div>
   
